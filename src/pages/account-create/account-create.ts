cab639773909bd6b4d05839ccee34a2d1b9a17f8
UlisesCeca <ulises@ulisesceca.com>
2018-07-04 21:28:55 +0200

    added form to create a new account

--- src/pages/account-create/account-create.ts
+++ src/pages/account-create/account-create.ts
@@ -1,12 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AccountCreatePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IonicPage, NavController, ViewController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -14,12 +8,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'account-create.html',
 })
 export class AccountCreatePage {
+@ViewChild('fileInput') fileInput;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  isReadyToSave: boolean;
+
+  item: any;
+
+  form: FormGroup;
+
+  constructor(public navCtrl: NavController, public viewCtrl: ViewController, formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      profilePic: [''],
+      name: ['', Validators.required],
+      about: ['']
+    });
+
+    // Watch the form for changes, and
+    this.form.valueChanges.subscribe((v) => {
+      this.isReadyToSave = this.form.valid;
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AccountCreatePage');
+
   }
 
+  processWebImage(event) {
+    let reader = new FileReader();
+    reader.onload = (readerEvent) => {
+
+      let imageData = (readerEvent.target as any).result;
+      this.form.patchValue({ 'profilePic': imageData });
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+  getProfileImageStyle() {
+    return 'url(' + this.form.controls['profilePic'].value + ')'
+  }
+
+  /**
+   * The user cancelled, so we dismiss without sending data back.
+   */
+  cancel() {
+    this.viewCtrl.dismiss();
+  }
+
+  /**
+   * The user is done and wants to create the item, so return it
+   * back to the presenter.
+   */
+  done() {
+    if (!this.form.valid) { return; }
+    this.viewCtrl.dismiss(this.form.value);
+  }
 }
